14cd70b994c1aa78e3f82c421ba3b5c943485747
Hohichh <verkovich2005@gmail.com>
2025-12-29 05:24:10 +0300

    fix proeprty for kafka

---
+++ init-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    CREATE DATABASE auth_db;
+    CREATE DATABASE user_db;
+    CREATE DATABASE order_db;
+EOSQL
